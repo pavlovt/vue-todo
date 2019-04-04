41f02db045194d75c62036ace7bb627d1546823f
Todor Pavlov <tpavlov@softserveinc.com>
2019-04-04 14:49:41 +0300

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p>Count: {{ $store.state.count }}</p>
+    <p>
+      <button @click="$store.commit('increment')">+</button>
+      <button @click="$store.commit('decrement')">-</button>
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+}
+</script>
